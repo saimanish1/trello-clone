98d573dac17cdd4173ee1bf1fbb3debd4da5fdbb
Sai Manish <manish.naganuri1@gmail.com>
2019-02-12 01:25:26 +0530

    fixed not changing the route to newly created board

--- client/src/Boards/Boards.js
+++ client/src/Boards/Boards.js
@@ -1,99 +1,115 @@
-import React, {Component} from 'react';
-import {Link, withRouter} from "react-router-dom";
+import React, { Component } from 'react';
+import { Link, withRouter } from 'react-router-dom';
 
 import classes from './Boards.module.scss';
-import {connect} from 'react-redux';
-import {getAllBoards} from "../store/actions/listActions";
-import Backdrop from "../components/Backdrop/Backdrop";
-import {addBoard} from "../store/actions/boardActions";
+import { connect } from 'react-redux';
+import { getAllBoards } from '../store/actions/listActions';
+import Backdrop from '../components/Backdrop/Backdrop';
+import { addBoard } from '../store/actions/boardActions';
 
 class Boards extends Component {
+  state = {
+    newBoardTitle: '',
+    isNewBoardTitleInEdit: false,
+  };
 
-    state = {
-        newBoardTitle: '',
-        isNewBoardTitleInEdit: false
-    };
+  onAddBoardHandler = () => {
+    this.setState({ isNewBoardTitleInEdit: true });
+  };
 
-    onAddBoardHandler = () => {
-
-        this.setState({isNewBoardTitleInEdit: true});
-
-
-    };
-
-    onBoardTitleChangeHandler = (e) => {
-        this.setState({newBoardTitle: e.target.value});
-    };
-    onBoardTitleOutsideClicked = () => {
-        this.setState({isNewBoardTitleInEdit: false});
-    };
-    onKeyDownAddBoardTitleHandler =(e)=>{
-       if(e.keyCode===13){
-          this.onBoardTitleChangeHandler(e);
-          this.onBoardTitleOutsideClicked();
-          this.props.addBoard(this.state.newBoardTitle,this.props.history);
-           this.setState({newBoardTitle:''});
-       }
-    };
-
-    componentDidMount() {
-
-        this.props.getAllBoards();
+  onBoardTitleChangeHandler = e => {
+    this.setState({ newBoardTitle: e.target.value });
+  };
+  onBoardTitleOutsideClicked = () => {
+    this.setState({ isNewBoardTitleInEdit: false });
+  };
+  onKeyDownAddBoardTitleHandler = e => {
+    if (e.keyCode === 13) {
+      this.onBoardTitleChangeHandler(e);
+      this.onBoardTitleOutsideClicked();
+      this.props.addBoard(this.state.newBoardTitle, this.props.history);
+      this.setState({ newBoardTitle: '' });
     }
-    
+  };
 
-    boardWrapper = () => {
-        if (this.state.isNewBoardTitleInEdit) {
-            return (<textarea ref={this.myRef} value={this.state.newBoardTitle} placeholder={"add a board title"}
-                         autoFocus   onChange={this.onBoardTitleChangeHandler} className={classes.add_board__input}
-                              onKeyDown={this.onKeyDownAddBoardTitleHandler}
-            />)
-        }
-        else {
-            return (
-                <p className={classes.board__item} onClick={this.onAddBoardHandler}><span
-                    className={classes.board__item__title}>Add a board</span></p>
-            )
-        }
-    };
+  componentDidMount() {
+    this.props.getAllBoards();
+  }
 
-    render() {
+  boardWrapper = () => {
+    if (this.state.isNewBoardTitleInEdit) {
+      return (
+        <textarea
+          ref={this.myRef}
+          value={this.state.newBoardTitle}
+          placeholder={'add a board title'}
+          autoFocus
+          onChange={this.onBoardTitleChangeHandler}
+          className={classes.add_board__input}
+          onKeyDown={this.onKeyDownAddBoardTitleHandler}
+        />
+      );
+    } else {
+      return (
+        <p className={classes.board__item} onClick={this.onAddBoardHandler}>
+          <span className={classes.board__item__title}>Add a board</span>
+        </p>
+      );
+    }
+  };
 
-        const {boards} = this.props;
-        let boardList = <p>Not yet retrieved</p>;
-        if (boards) {
-            if (boards.length > 0) {
-             boardList = boards.map((board, index) => (<Link key={board._id} to={`/boards/${board._id}`}
-                                                                className={classes.board__item}>{board.title}</Link>))
-            }
-            else {
-                boardList= (
-                    <React.Fragment>
-                        <h3 className={classes.board__empty__title}>No boards were created</h3>
-                        <Backdrop
-                            clickedOutside={this.onBoardTitleOutsideClicked}> {this.boardWrapper()}</Backdrop>
-
-                    </React.Fragment>)
-            }
-        }
-
-        return (
-            <div>
-
-                <div className={classes.board}>
-                    {boardList}
-                    <Backdrop
-                        clickedOutside={this.onBoardTitleOutsideClicked}> {this.boardWrapper()}</Backdrop>
-                </div>
-            </div>
+  render() {
+    const { boards } = this.props;
+    let boardList = <p>Not yet retrieved</p>;
+    if (boards) {
+      if (boards.length > 0) {
+        boardList = boards.map((board, index) => (
+          <Link
+            key={board._id}
+            to={`/boards/${board._id}`}
+            className={classes.board__item}
+          >
+            {board.title}
+          </Link>
+        ));
+      } else {
+        boardList = (
+          <React.Fragment>
+            <h3 className={classes.board__empty__title}>
+              No boards were created
+            </h3>
+            <Backdrop clickedOutside={this.onBoardTitleOutsideClicked}>
+              {' '}
+              {this.boardWrapper()}
+            </Backdrop>
+          </React.Fragment>
         );
+      }
     }
+
+    return (
+      <div>
+        <div className={classes.board}>
+          {boardList}
+          <Backdrop clickedOutside={this.onBoardTitleOutsideClicked}>
+            {' '}
+            {this.boardWrapper()}
+          </Backdrop>
+        </div>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = (state) => {
-    return {
-        boards: state.boards
-    }
+const mapStateToProps = state => {
+  return {
+    boards: state.boards,
+  };
 };
 
-export default withRouter(connect(mapStateToProps, {getAllBoards,addBoard})(Boards));
+export default withRouter(
+  connect(
+    mapStateToProps,
+    { getAllBoards, addBoard }
+  )(Boards)
+);
--- client/src/store/actions/boardActions.js
+++ client/src/store/actions/boardActions.js
@@ -1,13 +1,14 @@
-import instance from "../../axios-users";
+import instance from '../../axios-users';
 
-export const addBoard = (title,history)=>{
-  return  async dispatch=>{
-      try
-     { const res=await instance.post('/boards',{title});
-      console.log(res.data);
-     }
-     catch (e) {
-         console.log(e.response);
-     }
-  }  
+export const addBoard = (title, history) => {
+  return async dispatch => {
+    try {
+      const res = await instance.post('/boards', { title });
+      // history.push(`/boards/${}`)
+      const { _id: boardId } = res.data.result;
+      history.push(`/boards/${boardId}`);
+    } catch (e) {
+      console.log(e.response);
+    }
+  };
 };
